de0224543122cba94033499cadc9c01dfdc8a45e
ShahafDavid96 <89782730+ShahafDavid96@users.noreply.github.com>
2023-06-17 14:05:37 +0300

    Update graph2.py

--- graph2.py
+++ graph2.py
@@ -165,7 +165,7 @@ def get_fig(Tm, ps):
         tickfont=dict(size=14, color='#000000')
     ),
     yaxis=dict(
-        title='Mean of The Stats',
+        title='Mean of Stats',
         titlefont=dict(size=16, color='#000000'),
         tickfont=dict(size=14, color='#000000'),
         showgrid=True, gridwidth=0.2, gridcolor='#D7DBDD'
